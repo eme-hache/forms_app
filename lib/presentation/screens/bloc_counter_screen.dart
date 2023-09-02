d179fcefef6007645e80bb948dd9795464fe7253
Eme Hache <luisalfonsodsg@outlook.com>
2023-09-02 15:26:56 -0400

    initial commit

---
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class BlocCounterScreen extends StatelessWidget {
+  const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contador con BLoC'),
+        actions: [
+          IconButton(
+              onPressed: () => {}, icon: const Icon(Icons.refresh_rounded))
+        ],
+      ),
+      body: const Center(
+        child: Text('Valor del contador: 0'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1',
+            onPressed: () => {},
+            child: const Text('+3'),
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: '2',
+            onPressed: () => {},
+            child: const Text('+2'),
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: '3',
+            onPressed: () => {},
+            child: const Text('+1'),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CubitCounterScreen extends StatelessWidget {
+  const CubitCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contador con cubits'),
+        actions: [
+          IconButton(
+              onPressed: () => {}, icon: const Icon(Icons.refresh_rounded))
+        ],
+      ),
+      body: const Center(
+        child: Text('Valor del contador: 0'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1',
+            onPressed: () => {},
+            child: const Text('+3'),
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: '2',
+            onPressed: () => {},
+            child: const Text('+2'),
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: '3',
+            onPressed: () => {},
+            child: const Text('+1'),
+          ),
+        ],
+      ),
+    );
+  }
+}
